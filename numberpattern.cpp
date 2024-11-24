1d458e4d4e2b3492348aa0aa11a8fc53d6999d9e
santhosh-ram <santhoshram6110@gmail.com>
2024-11-24 17:04:30 +0530

    Added new file

---
+++ numberpattern.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    int l[a];
+    for(int i=0;i<n;i++)
+    {
+        cin >> l[i];
+    }
+    int m;
+    cin >> m;
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<i+1;j++)
+        {
+            cout << l[c%a];
+            c++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
