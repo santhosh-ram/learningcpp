4e6e2d1e4e3b8db48de1189a359de9bf866105d7
santhosh-ram <santhoshram6110@gmail.com>
2024-12-12 21:00:24 +0530

    Added new file

---
+++ equalornotusingbitwise.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int num1, num2;
+    cout << "Enter the first number: ";
+    cin >> num1;
+    cout << "Enter the second number: ";
+    cin >> num2;
+    if ((num1 ^ num2) == 0)
+     {
+        cout << "The numbers are equal." << endl;
+    }
+    else
+    {
+        cout << "The numbers are not equal." << endl;
+    }
+
+    return 0;
+}
